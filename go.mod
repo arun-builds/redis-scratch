7c5c118e211d3a29ac88e0f6d9ad265bcdec2252
arunprajapati-arch <arunprajapati.mail@gmail.com>
2026-05-14 21:41:10 +0530

    simple echo server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/arun-builds/redis-scratch
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/arun-builds/redis-scratch/config"
+	"github.com/arun-builds/redis-scratch/server"
+)
+
+func setupFlags() {
+	flag.StringVar(&config.Host, "host", "0.0.0.0", "host for the redis server")
+	flag.IntVar(&config.Port, "port", 6969, "port for redis server")
+	flag.Parse()
+}
+
+func main() {
+	setupFlags()
+	log.Printf("starting redis")
+	server.RunSyncTCPServer()
+}
