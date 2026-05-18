ac1a3bb267d6a129119cf37662ff55194fbfda84
arunprajapati-arch <arunprajapati.mail@gmail.com>
2026-05-18 21:06:42 +0530

    implemented GET, SET, and TTL

--- core/resp.go
+++ core/resp.go
@@ -131,6 +131,9 @@ func Encode(value interface{}, isSImple bool) []byte {
 			return []byte(fmt.Sprintf("+%s\r\n", v))
 		}
 		return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(v), v))
+	case int64:
+		return []byte(fmt.Sprintf(":%d\r\n", v))
+	default:
+		return RESP_NIL
 	}
-	return []byte{}
 }
