494658bf8093b060725b90e0f4c88ad3fdd221ba
arunprajapati-arch <arunprajapati.mail@gmail.com>
2026-05-28 17:07:39 +0530

    AOF implementation

---
+++ core/aof.go
@@ -0,0 +1,36 @@
+package core
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/arun-builds/redis-scratch/config"
+)
+
+// TODO: Support Expiration
+// TODO: Support non-kv data structures
+// TODO: Support sync write
+
+func dumpKey(fp *os.File, key string, obj *Obj) {
+	cmd := fmt.Sprintf("SET %s %s", key, obj.Value)
+	tokens := strings.Split(cmd, " ")
+	fp.Write(Encode(tokens, false))
+}
+
+// TODO: To to new and switch
+
+func DumpAllAOF() {
+	fp, err := os.OpenFile(config.AOFFile, os.O_CREATE|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		fmt.Print("error", err)
+		return
+	}
+	defer fp.Close()
+	log.Println("rewriting AOF file at", config.AOFFile)
+	for k, obj := range store {
+		dumpKey(fp, k, obj)
+	}
+	log.Println("AOF file rewrite complete")
+}
