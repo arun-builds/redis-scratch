f14fd5db8c8ae96b6f3c400928615ce482ed334c
arunprajapati-arch <arunprajapati.mail@gmail.com>
2026-06-01 08:07:17 +0530

    INFO and allkeys-random key eviction implementation

--- core/eviction.go
+++ core/eviction.go
@@ -11,10 +11,28 @@ func evictFirst() {
 	}
 }
 
+// Randomly removes keys to make space for the new data added
+// the number of keys removed will be sufficient to free up least 10% space
+func evictAllKeysRandom() {
+	evictCount := int64(config.EvictionRatio * float64(config.KeysLimit))
+	//iteration of golang dictionary can be considered as a random
+	// because it depends on the hash of the inserted key
+	for k := range store {
+		Del(k)
+		evictCount--
+		if evictCount <= 0 {
+			break
+		}
+	}
+}
+
 // TODO: Support multiple eviction strategies
 func evict() {
 	switch config.EvictionStrategy {
 	case "simple-first":
 		evictFirst()
+	case "allkeys-random":
+		evictAllKeysRandom()
 	}
+
 }
